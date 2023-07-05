add009ed3855fd00aa51215197d61e2f76393246
parteek kumar <digitalparteek@gmail.com>
2023-07-06 01:44:53 +0530

    Added blog Link

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,25 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Styles from "../../styles/blogPost.module.css";
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>Whats'up {slug}</div>;
+  return (
+    <>
+      <main className={Styles.main}>
+        <div>Whats'up {slug}</div>
+        <h1>Title of the page: {slug}</h1>
+        <hr />
+        <div>
+          Nulla Lorem eiusmod reprehenderit laborum laboris consequat esse
+          officia exercitation voluptate nisi ut. Non nulla sit deserunt
+          cupidatat qui non. Dolore et ea aliquip enim velit officia excepteur
+          est voluptate nulla qui.
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default slug;
